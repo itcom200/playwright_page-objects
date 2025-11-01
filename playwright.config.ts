94cc6920730b1358f900efebfc5a1ccb8d4e1fc7
suphanat <suphanat0809@gmail.com>
2025-11-02 06:51:38 +0700

    test ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,54 +20,56 @@ export default defineConfig({
   /* ถ้ารันบน CI ให้ retry test ที่ fail ได้สูงสุด 2 รอบ */
   workers: process.env.CI ? 1 : undefined,
   /* บน CI ให้รันทีละ 1 worker (กัน resource ล้นเครื่อง) */
-  reporter: 'html',
+  reporter: [['html'], ['allure-playwright']],
   /* ใช้ HTML report เพื่อดูผลการทดสอบผ่าน browser (npx playwright show-report) */
-  timeout:25000,
+  timeout: 25000,
   /*Run เกิน 15s ถ้ายังไม่จบ test จะ (failed) และหยุดทำงานทันที*/
-/**************************************************************************************************/
+  /**************************************************************************************************/
   /* กำหนด ค่าการตั้งค่าเริ่มต้น สำหรับแต่ละ test run เช่น no.UI , ขนาด browser , baaseURL ไม่ใส่ / */
   use: {
-//เอาไว้ดูย้อนหลังว่า test ทำอะไรบ้างผ่าน UI แบบ interactive
-//off ,on ,retain-on-failure*เก็บตอนFail ,on-first-retry*เก็บตอน retry(รีทาย) ลองใหม่อีกครั้ง
+    //เอาไว้ดูย้อนหลังว่า test ทำอะไรบ้างผ่าน UI แบบ interactive
+    //off ,on ,retain-on-failure*เก็บตอนFail ,on-first-retry*เก็บตอน retry(รีทาย) ลองใหม่อีกครั้ง
     trace: 'retain-on-failure',  //เทรด(ติดตาม) 
-    
-//VDO จะอยู่ที่ playwright-report/data แค่ชั่วคราวถ้า RunTest ใหม่มันจะหาย 
-//เก็บจริงที่ test-results จะไม่หาย testcase จะไม่ซ้ำกันถ้า run test ซ้ำจะอันเก่าจะหาย แต่ถ้า test ใหม่จะเก็บ
-// video: 'retain-on-failure',//-->off=ไม่อัด , on=อัดทุกTest , retain-on-failure=อันตอนFail , on-first-retry=อัดครั้งแรก
-    video:'off', 
 
-    headless:false, //สั่งให้เปิด browser แบบ แสดงผล (มี UI) ขณะทดสอบ
+    //VDO จะอยู่ที่ playwright-report/data แค่ชั่วคราวถ้า RunTest ใหม่มันจะหาย 
+    //เก็บจริงที่ test-results จะไม่หาย testcase จะไม่ซ้ำกันถ้า run test ซ้ำจะอันเก่าจะหาย แต่ถ้า test ใหม่จะเก็บ
+    // video: 'retain-on-failure',//-->off=ไม่อัด , on=อัดทุกTest , retain-on-failure=อันตอนFail , on-first-retry=อัดครั้งแรก
+    video: 'off',
 
-// viewport: { width: 1280, height: 720 },//--> กำหนดขนาด browser ใช้ได้กับทุกอันที่ Playwright รองรับ
-// screenshot: 'only-on-failure',//-->off=ไม่ถ่าย , on=ถ่ายทุกครั้ง , only-on-failure=ถ่ายตอนFail-->เก็บไฟล์ test-results/
+    headless: false, //สั่งให้เปิด browser แบบ แสดงผล (มี UI) ขณะทดสอบ
+
+    // viewport: { width: 1280, height: 720 },//--> กำหนดขนาด browser ใช้ได้กับทุกอันที่ Playwright รองรับ
+    // screenshot: 'only-on-failure',//-->off=ไม่ถ่าย , on=ถ่ายทุกครั้ง , only-on-failure=ถ่ายตอนFail-->เก็บไฟล์ test-results/
     // baseURL:'https://www.carwale.com/' //วิธีเรียกใช้ await page.goto('/') ใส่แค่นี้หรือจะใส่พาสตามหลังก็ได้
   },
-/**************************************************************************************************/
+  /**************************************************************************************************/
   /* Configure projects for major browsers */
-    // Run test 2 รอบ รอบละ browser คือ Chrome (ผ่าน Chromium) และ Firefox
-    // แต่ละรอบจะใช้ preset device ที่ Playwright เตรียมไว้ เช่น desktop viewport, user-agent เป็นต้น
+  // Run test 2 รอบ รอบละ browser คือ Chrome (ผ่าน Chromium) และ Firefox
+  // แต่ละรอบจะใช้ preset device ที่ Playwright เตรียมไว้ เช่น desktop viewport, user-agent เป็นต้น
   projects: [
     {
       name: 'chromium',
       // testMatch:/.*chrome\.spec\.ts/, 
-      use: { ...devices['Desktop Chrome']
-      //channel คือตัวเลือกเสริม ต้องใส่คู่กับ browserName ถ้าไม่กำหนดตัวเสริมมันจะอ่านแค่ browserName
-        ,browserName:'chromium',//ใช้ Chromium ที่ Playwright โหลดมาเอง ต้องใช้คู่กับ channel
-        channel:'chrome',//ใช้ Chrome ตัวเต็มจากเครื่อง ถ้าไม่ใส่ channel จะใช้ Chromium ที่ Playwright โหลดมา
-    // browserName:'firefox',
+      use: {
+        ...devices['Desktop Chrome']
+        //channel คือตัวเลือกเสริม ต้องใส่คู่กับ browserName ถ้าไม่กำหนดตัวเสริมมันจะอ่านแค่ browserName
+        , browserName: 'chromium',//ใช้ Chromium ที่ Playwright โหลดมาเอง ต้องใช้คู่กับ channel
+        channel: 'chrome',//ใช้ Chrome ตัวเต็มจากเครื่อง ถ้าไม่ใส่ channel จะใช้ Chromium ที่ Playwright โหลดมา
+        // browserName:'firefox',
       },
     },
 
     {
       name: 'firefox',
       //สำหรับ match ชื่อไฟล์ test ที่ต้องรัน รันเฉพาะไฟล์ที่ลงท้ายด้วย file_test.spec.ts
-      testMatch:/.*firefox\.spec\.ts/, 
-      use: { ...devices['Desktop Firefox'] 
-        ,browserName:'firefox',
-        channel:'firefox',
+      testMatch: /.*firefox\.spec\.ts/,
+      use: {
+        ...devices['Desktop Firefox']
+        , browserName: 'firefox',
+        channel: 'firefox',
       },
     },
-/**************************************************************************************************/
+    /**************************************************************************************************/
     // {
     //   name: 'webkit',
     //   use: { ...devices['Desktop Safari'] },
