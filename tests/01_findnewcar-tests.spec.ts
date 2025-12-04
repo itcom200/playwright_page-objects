fa10d0428cc8ead31522380a2e021f1d98270565
suphanat <suphanat0809@gmail.com>
2025-12-05 06:34:51 +0700

    test

--- tests/01_findnewcar-tests.spec.ts
+++ tests/01_findnewcar-tests.spec.ts
@@ -14,57 +14,57 @@ test.describe('Find New Car', () => {
         await expect(headerText).toContain('New Cars');
     });
 
-    const testData = readCSV('data/testdata.csv',true)
-    test('Parameterized Find New Car', async ({ pages }) => {
-        for (const data of testData) {
-            await pages.homePage.findNewCar();
-            await expect(pages.page).toHaveURL(/.*new-cars/);
+    // const testData = readCSV('data/testdata.csv',true)
+    // test('Parameterized Find New Car', async ({ pages }) => {
+    //     for (const data of testData) {
+    //         await pages.homePage.findNewCar();
+    //         await expect(pages.page).toHaveURL(/.*new-cars/);
             
-            if (data.carBrand === 'bmw') {
-                await pages.newCarsPage.gotoBMWCar();
-                await expect(pages.page).toHaveURL(/.*bmw-cars/);
-            } else if (data.carBrand === 'honda') {
-                await pages.newCarsPage.gotoHondaCar();
-                await expect(pages.page).toHaveURL(/.*honda-cars/);
-            } else if (data.carBrand === 'hyundai') {
-                await pages.newCarsPage.gotoHyundaiCar();
-                await expect(pages.page).toHaveURL(/.*hyundai-cars/);
-            } else if (data.carBrand === 'toyota') {
-                await pages.newCarsPage.gotoToyotaCar();
-                await expect(pages.page).toHaveURL(/.*toyota-cars/);
-            }
-            const carTitle=await pages.carBase.getCarTitle();
-            console.log(`Car Title : ${carTitle}`);
-            expect(carTitle.toUpperCase()).toContain(data.carTitle.toUpperCase());
-        };
-    });
+    //         if (data.carBrand === 'bmw') {
+    //             await pages.newCarsPage.gotoBMWCar();
+    //             await expect(pages.page).toHaveURL(/.*bmw-cars/);
+    //         } else if (data.carBrand === 'honda') {
+    //             await pages.newCarsPage.gotoHondaCar();
+    //             await expect(pages.page).toHaveURL(/.*honda-cars/);
+    //         } else if (data.carBrand === 'hyundai') {
+    //             await pages.newCarsPage.gotoHyundaiCar();
+    //             await expect(pages.page).toHaveURL(/.*hyundai-cars/);
+    //         } else if (data.carBrand === 'toyota') {
+    //             await pages.newCarsPage.gotoToyotaCar();
+    //             await expect(pages.page).toHaveURL(/.*toyota-cars/);
+    //         }
+    //         const carTitle=await pages.carBase.getCarTitle();
+    //         console.log(`Car Title : ${carTitle}`);
+    //         expect(carTitle.toUpperCase()).toContain(data.carTitle.toUpperCase());
+    //     };
+    // });
 
-    test('Car Name and Price Test', async ({ pages }) => {
-        for (const data of testData) {
-            await pages.homePage.findNewCar();
-            await expect(pages.page).toHaveURL(/.*new-cars/);
+    // test('Car Name and Price Test', async ({ pages }) => {
+    //     for (const data of testData) {
+    //         await pages.homePage.findNewCar();
+    //         await expect(pages.page).toHaveURL(/.*new-cars/);
             
-            if (data.carBrand === 'bmw') {
-                await pages.newCarsPage.gotoBMWCar();
-                await expect(pages.page).toHaveURL(/.*bmw-cars/);
-            } else if (data.carBrand === 'honda') {
-                await pages.newCarsPage.gotoHondaCar();
-                await expect(pages.page).toHaveURL(/.*honda-cars/);
-            } else if (data.carBrand === 'hyundai') {
-                await pages.newCarsPage.gotoHyundaiCar();
-                await expect(pages.page).toHaveURL(/.*hyundai-cars/);
-            } else if (data.carBrand === 'toyota') {
-                await pages.newCarsPage.gotoToyotaCar();
-                await expect(pages.page).toHaveURL(/.*toyota-cars/);
-            }
-            const carTitle=await pages.carBase.getCarTitle();
-            console.log(`Car Title : ${carTitle}`);
-            expect(carTitle.toUpperCase()).toContain(data.carTitle.toUpperCase());
+    //         if (data.carBrand === 'bmw') {
+    //             await pages.newCarsPage.gotoBMWCar();
+    //             await expect(pages.page).toHaveURL(/.*bmw-cars/);
+    //         } else if (data.carBrand === 'honda') {
+    //             await pages.newCarsPage.gotoHondaCar();
+    //             await expect(pages.page).toHaveURL(/.*honda-cars/);
+    //         } else if (data.carBrand === 'hyundai') {
+    //             await pages.newCarsPage.gotoHyundaiCar();
+    //             await expect(pages.page).toHaveURL(/.*hyundai-cars/);
+    //         } else if (data.carBrand === 'toyota') {
+    //             await pages.newCarsPage.gotoToyotaCar();
+    //             await expect(pages.page).toHaveURL(/.*toyota-cars/);
+    //         }
+    //         const carTitle=await pages.carBase.getCarTitle();
+    //         console.log(`Car Title : ${carTitle}`);
+    //         expect(carTitle.toUpperCase()).toContain(data.carTitle.toUpperCase());
 
-            const cars=await pages.carBase.getCarNameAndPrice();
-            console.log(cars);
-        };
-    });
+    //         const cars=await pages.carBase.getCarNameAndPrice();
+    //         console.log(cars);
+    //     };
+    // });
 
 
 
